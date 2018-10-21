110494798bb5b7f33355d6099cc01aaa77c3df30
Maas Lalani <42545625+maaslalani@users.noreply.github.com>
2018-10-21 05:25:23 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,8 +2,8 @@ import os
 
 def scrape(username, destination='temp'):
     MAX_POSTS = str(100)
-    LOGIN_USER = "jonathangin52"
-    LOGIN_PASS = "mBbf^8Kq2sc8"
+    LOGIN_USER = "ENTER_YOUR_USERNAME"
+    LOGIN_PASS = "ENTER_YOUR_PASSWORD"
 
     COMMAND = "instagram-scraper " + username + " -u " + LOGIN_USER + " -p " + LOGIN_PASS + " --destination " + destination + " --media-type image --media-metadata --maximum " + MAX_POSTS
     os.system(COMMAND)
